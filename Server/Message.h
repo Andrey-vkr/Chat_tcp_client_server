cba3d8f508fea227f479122571ff223ebbcd14b7
Andrey <and.vkr@mail.ru>
2024-04-12 03:00:22 +0300

    4_file_

---
+++ Server/Message.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <string>
+
+
+class Message
+{
+    std::string _from;
+    std::string _to;
+    std::string _text;
+
+public:
+    Message(const std::string& from, const std::string& to, const std::string& text) :
+        _from(from),
+        _to(to),
+        _text(text)
+    {}
+
+    const std::string& getFrom() const
+    {
+        return _from;
+    }
+    const std::string& getTo() const
+    {
+        return _to;
+    }
+    const std::string& getText() const
+    {
+        return _text;
+    }
+};
+
