d74cb7188d1adb55cb15de8947843212ab24f76a
Andrey <and.vkr@mail.ru>
2024-04-12 03:01:13 +0300

    6_file_

---
+++ Server/Server.h
@@ -0,0 +1,26 @@
+#pragma once
+#include<iostream>
+#include<unistd.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+
+#define MESSAGE_LENGTH 1024
+#define PORT 7777
+
+class Server
+{
+public:
+    struct sockaddr_in serveraddress, client;
+    socklen_t length;
+    int sockert_file_descriptor, connection, bind_status, connection_status;
+    char message[MESSAGE_LENGTH];
+
+    void createSocket();
+    void bindSocket();
+    void closeSocket();
+    void dataReceivingServer();
+    char* readData();
+    void writeData(std::string serverMessage);
+};
